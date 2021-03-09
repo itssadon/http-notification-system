d08486bc4af9f78fe1c575e8a64f3e1c1b4fc79e
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-03-09 16:06:31 +0100

    Add index test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('App', function () {
+  it('has the default page', function (done) {
+    request(app)
+      .get('/')
+      .expect(/Welcome to Express/, done);
+  });
+});
