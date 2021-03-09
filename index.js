677da4928550ae0741b253105a106c8b86d55f29
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-03-09 16:03:11 +0100

    Minor code refactoring

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import createError from 'http-errors';
-import express, { json, urlencoded, static } from 'express';
-import { join } from 'path';
+import express, {
+    json,
+    urlencoded,
+    static
+} from 'express';
+import {
+    join
+} from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -14,26 +20,28 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 export default app;
